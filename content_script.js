ef93510132e7cb08fadfd3f875005e6c6c52fce5
notsiva <sivakarankpr15@gmail.com>
2025-06-01 16:23:52 +0530

    added-comments

--- content_script.js
+++ content_script.js
@@ -1,19 +1,20 @@
 (function () {
-  const container = document.createElement("div");
+  const container = document.createElement("div"); //creating a container
   container.id = "anime-image-container";
 
-  const image = document.createElement("img");
+  const image = document.createElement("img"); //creating a img element
   image.id = "anime-image";
-  container.appendChild(image);
-  document.body.appendChild(container);
+
+  container.appendChild(image); //insert a child inside the container
+  document.body.appendChild(container); //insert the container inside the body of the html
 
   let timeoutId;
-  let toggle = true;
+  let toggle = true; //set the default toggle as true
 
   function toggleImage() {
-    const imageFile = toggle ? "assets/a.png" : "assets/b.png";
+    const imageFile = toggle ? "assets/a.png" : "assets/b.png"; //toggle the image to a if not false and vice versa
     image.src = chrome.runtime.getURL(imageFile);
-    container.style.display = "block";
+    container.style.display = "block"; // set the image style to block
 
     clearTimeout(timeoutId);
     timeoutId = setTimeout(() => {
